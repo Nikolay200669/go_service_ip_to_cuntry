80b6fed456fb0a94bfa17a4e4749eafa5dc8361a
Nike <nikolay200669@gmail.com>
2017-09-25 11:11:01 +0300

    New dump DB

--- dump.sql
+++ dump.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `test` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `test`;
 -- MySQL dump 10.13  Distrib 5.7.13, for linux-glibc2.5 (x86_64)
 --
 -- Host: 127.0.0.1    Database: test
@@ -26,10 +24,12 @@ DROP TABLE IF EXISTS `country`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `country` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `country` varchar(45) DEFAULT NULL,
-  `ip` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `country` varchar(45) NOT NULL,
+  `ip` varchar(45) NOT NULL,
+  `validityip` int(11) DEFAULT NULL,
+  `inserted` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`,`ip`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +38,7 @@ CREATE TABLE `country` (
 
 LOCK TABLES `country` WRITE;
 /*!40000 ALTER TABLE `country` DISABLE KEYS */;
-INSERT INTO `country` VALUES (1,'US','8.8.4.4');
+INSERT INTO `country` VALUES (19,'US','8.8.8.8',10,1506326176);
 /*!40000 ALTER TABLE `country` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -63,7 +63,7 @@ CREATE TABLE `url` (
 
 LOCK TABLES `url` WRITE;
 /*!40000 ALTER TABLE `url` DISABLE KEYS */;
-INSERT INTO `url` VALUES (1,'http://ip-api.com/json/',1506180497),(2,'http://geoip.nekudo.com/api/',1),(3,'http://freegeoip.net/json/',2);
+INSERT INTO `url` VALUES (1,'http://ip-api.com/json/',1506326176),(2,'http://geoip.nekudo.com/api/',1),(3,'http://freegeoip.net/json/',2);
 /*!40000 ALTER TABLE `url` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -74,6 +74,39 @@ UNLOCK TABLES;
 --
 -- Dumping routines for database 'test'
 --
+/*!50003 DROP FUNCTION IF EXISTS `get_country_function` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `get_country_function`(in_ip VARCHAR(45)) RETURNS varchar(45) CHARSET utf8
+BEGIN
+	DECLARE out_country varchar(45);
+    DECLARE out_validityip int(11);
+    DECLARE out_inserted int(10);
+    
+	SELECT country, validityip, inserted INTO out_country, out_validityip, out_inserted FROM country WHERE ip = in_ip;
+    IF validityip > 0 THEN
+		IF UNIX_TIMESTAMP() - out_inserted < out_validityip THEN
+			DELETE FROM country WHERE ip = in_ip;
+			RETURN ('');
+		ELSE
+			RETURN (out_country);
+		END IF;
+	ELSE
+		RETURN ('');
+	END IF;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP FUNCTION IF EXISTS `get_url_function` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -88,16 +121,13 @@ CREATE DEFINER=`root`@`localhost` FUNCTION `get_url_function`(range_sec INT) RET
 BEGIN
 	DECLARE out_url varchar(100);
     DECLARE out_time int(10);
-    -- DECLARE range_sec int(2);
-    
-    -- SET range_sec = 10;
     
 
 	SELECT url.url, url.time_at INTO out_url, out_time FROM test.url WHERE UNIX_TIMESTAMP() - url.time_at > range_sec LIMIT 1;
     IF UNIX_TIMESTAMP() - out_time > range_sec THEN
 		UPDATE test.url SET url.time_at = UNIX_TIMESTAMP() WHERE url.url = out_url;
 	ELSE
-		SET out_url = 'nil';
+		SET out_url = '';
 	END IF;
 RETURN (out_url);
 END ;;
@@ -116,4 +146,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-09-23 18:31:41
+-- Dump completed on 2017-09-25 11:06:13
